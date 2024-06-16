26551c22eb9f626a23bf76fcbae0524e34bd8508
Trần Minh Đức <143236222+Minhductran0912@users.noreply.github.com>
2024-06-17 00:05:45 +0700

    commit PHP

---
+++ Booking-Tickets-Website/routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\TicketController;
+
+Route::get('/',[TicketController::class,'index']);
+Route::get('/tickets', [TicketController::class, 'show']);
+Route::post('/book', [TicketController::class, 'book']);
